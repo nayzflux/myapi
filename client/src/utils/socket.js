9509c8d30da174d20f95016b4059ff2021dd07c4
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-05-15 19:53:42 +0200

    fix wss

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "ws://api.chatapp.nayz.fr", {
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "wss://api.chatapp.nayz.fr", {
     autoConnect: false,
     withCredentials: true
 })
