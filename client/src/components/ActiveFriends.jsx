efa90c5c2d7ac71a258d55f8b9b9c45afb56cdee
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-05-16 07:14:13 +0200

    create conv on click on user active

--- client/src/components/ActiveFriends.jsx
+++ client/src/components/ActiveFriends.jsx
@@ -1,11 +1,14 @@
 import { userState } from '@/atoms/userAtom';
+import { createConversation } from '@/utils/api';
 import { socket } from '@/utils/socket';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil';
 
 const ActiveFriends = () => {
     const [onlines, setOnline] = useState([]);
     // const [user, setUser] = useRecoilState(userState);
+    const router = useRouter();
 
     useEffect(() => {
         socket.connect();
@@ -18,6 +21,27 @@ const ActiveFriends = () => {
             socket.off('user_disconnection', removeOnline);
         });
     }, []);
+
+    const handleConversationButton = (e, user) => {
+        e.preventDefault();
+        console.log("Handle Conversation button");
+
+        createConversation([user._id], null)
+            .then(conversation => {
+                // Si la conv est créer
+                if (conversation) {
+                    router.push(`/conversations/${conversation._id}`)
+                }
+            })
+            .catch(conversation => {
+                // Si la conv existe déjà
+                if (conversation) {
+                    router.push(`/conversations/${conversation._id}`)
+                } else {
+                    console.log("Error");
+                }
+            });
+    }
 
     const addOnline = (online) => {
         setOnline(old => old.find(u => u._id.toString() === online._id.toString()) ? old : [...old, online]);
@@ -32,7 +56,7 @@ const ActiveFriends = () => {
             <h3>Utilisateur en ligne</h3>
             <div className='flex flex-row space-x-1 overflow-auto'>
                 {onlines.map(user => (
-                    <div className='flex flex-col' key={user._id}>
+                    <div className='flex flex-col' key={user._id} onClick={(e) => handleConversationButton(e, user)}>
                         <img className='rounded-full w-12 h-12' src={user.picture?.url} alt="Photo de Profile" />
                         <p>{user.username}</p>
                     </div>
