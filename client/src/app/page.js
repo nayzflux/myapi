a00851c197bc3da91395e2fa17842ba708652ff3
legregz <132829354+legregz@users.noreply.github.com>
2023-05-17 18:09:33 +0200

    put the link in the sentence

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <div>
-        <p>Bienvenue sur ChatApp clique <a href='/conversations'>ici</a> pour commencer à discuter</p>
+        <p>Bienvenue sur ChatApp, clique <a href='/conversations'>ici</a> pour commencer à discuter</p>
       </div>
     </div>
   )
