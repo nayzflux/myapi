d142b521db49da1e28c7a99f3352edc2ac117e4d
legregz <132829354+legregz@users.noreply.github.com>
2023-05-18 18:52:39 +0200

    Update RegisterForm.jsx

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -177,7 +177,7 @@ const RegisterForm = () => {
 
                 {/* C */}
                 <div className='flex flex-row justify-end'>
-                    <input type='submit' value='CRÉER LE COMPTE' className='bg-blue-600 text-white font-semibold py-1 px-3 rounded-full text-lg hover:shadow-lg hover:scale-105 cursor-pointer ease-out transition-all duration-300' />
+                    <input type='submit' value='CRÉER LE COMPTE' className='bg-blue-600 text-white font-semibold py-1 px-3 rounded text-lg hover:shadow-lg active:scale-95 cursor-pointer ease-out transition-all duration-300' />
                 </div>
             </form>
 
