9f661a921880222a70b62aac8c987d14ae6eb363
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-05-18 17:01:03 +0200

    Style home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,13 +1,15 @@
 'use client';
 
 import React from 'react'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div className='flex flex-col justify-center items-center h-screen w-screen text-2xl'>
-        Bienvenue sur <stong className='font-bold text-blue-600 underline'>WaveChat</stong>, clique <a href='/conversations'>ici</a> pour commencer à discuter
+      <div className='flex flex-col justify-center h-screen items-center text-xl space-y-10 p-3'>
+          <h1>Bienvenue sur l'application de messsagerie <stong className='font-bold text-blue-600 underline'>WaveChat</stong><br/>Cliquer ci-dessous pour t'inscrire et commencer à discuter</h1>
+          <Link href="/conversations" >
+              <button className="rounded-full bg-blue-600 py-2 px-4 text-white font-semibold hover:shadow-2xl hover:scale-105 transition-all ease-out duration-300">C'est parti</button>
+          </Link>
       </div>
-    </div>
   )
 }
