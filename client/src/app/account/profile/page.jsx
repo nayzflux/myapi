18f6e47c4913f9ff54a69101fab4bcb855d09f89
legregz <132829354+legregz@users.noreply.github.com>
2023-05-18 18:23:43 +0200

    Update page.jsx

--- client/src/app/account/profile/page.jsx
+++ client/src/app/account/profile/page.jsx
@@ -101,11 +101,11 @@ const AccountPage = () => {
                         <input className='bg-transparent text-lg outline-none' type='text' placeholder={user?.picture?.url ? user.picture.url : "Entrer le lien d'une photo de profil..."} value={pictureUrl} onChange={handlePictureChange} />
                     </div >
 
-                    <div className='flex flex-col justify-between'>
-                        <div className='flex justify-start'>
+                    <div className='flex flex-row justify-between'>
+                        <div className='flex flex-row justify-start'>
                             <input type="button" className='text-blue-600 text-lg underline cursor-pointer' value="Annuler" onClick={handleCancel}/>
                         </div>
-                        <div className='flex justify-end'>
+                        <div className='flex flex-row justify-end'>
                             <input className='text-white bg-blue-600 py-1 px-3 font-semibold rounded text-lg hover:shadow-lg active:scale-95 cursor-pointer ease-out transition-all duration-300' type='submit' value="ENREGISTER" />
                         </div>
                     </div>
